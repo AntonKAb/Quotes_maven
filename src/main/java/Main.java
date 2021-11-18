beb4ada33524fbd66f98a5004a7354417ab29f2f
AntonKAb <anton_10_1999@mail.ru>
2021-11-18 13:07:29 +0700

    Added call function "java -jar".

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 public class Main {
@@ -14,41 +13,40 @@ public class Main {
     public static  String dateF = "";
     public static  String currencyF = "";
     public static  String valueF = "";
+    public static String inputDate = "";
+    public static String inputID = "";
 
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, SQLException {
 
+        String inputDate1 = "";
+        if(args[0].equals("updateDB")){
+            updateBD(args[1]);
+        } else {
+            inputDate1 = args[0];
+            inputID = args[1];
+        }
+        inputDate = dataCorrect(inputDate1);
         try {
 
             Connection connection = DriverManager.getConnection(url, user, "12124576");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Quoters");
 
-            while (true) {
-                System.out.println("Введите дату в формате dd/MM/yyyy: ");
-                System.out.println("(для выхода из программы введите 'Exit')");
-                Scanner input = new Scanner(System.in);
-                String inputDate = input.next();
-                exit(inputDate);
-
-                System.out.println("Введите идентификатор валюты: ");
-                String inputID = input.next();
-                exit(inputID);
-
-                try{
-                    ArrayList<String> dateList = new ArrayList<>();
-                    ArrayList<String> currencyList = new ArrayList<>();
-                    ArrayList<String> valueList = new ArrayList<>();
-                    while(resultSet.next()){
-                        dateList.add(resultSet.getString("DATE"));
-                        currencyList.add(resultSet.getString("CURRENCY"));
-                        valueList.add(resultSet.getString("VALUE"));
+            try{
+                ArrayList<String> dateList = new ArrayList<>();
+                ArrayList<String> currencyList = new ArrayList<>();
+                ArrayList<String> valueList = new ArrayList<>();
+                while(resultSet.next()){
+                    dateList.add(resultSet.getString("DATE"));
+                    currencyList.add(resultSet.getString("CURRENCY"));
+                    valueList.add(resultSet.getString("VALUE"));
                    }
 
-                    int find = 0;
-                    int index = 0;
-                    for(String date: dateList){
-                        if (date.equals(inputDate)){
+                int find = 0;
+                int index = 0;
+                for(String date: dateList){
+                    if (date.equals(inputDate)){
                             String cur = currencyList.get(index);
                             if (cur.equals(inputID)){
                                 dateF = date;
@@ -68,7 +66,7 @@ public class Main {
                         throw new SQLException();
 
                     }
-
+                connection.close();
                 } catch (Exception SQLException) {
                     Quotation.requestRates(inputDate, inputID);
 
@@ -82,13 +80,10 @@ public class Main {
 
                 }
             }
-
-        }
         catch (Exception ex) {
-            System.out.println("Connection failed...");
-        }
+        System.out.println("Connection failed...");
     }
-
+    }
 
 
     static void exit(String input) {
@@ -97,7 +92,16 @@ public class Main {
             System.exit(0);
         }
     }
-    public static void updateBD(){
+    public static void updateBD(String arg){
 
+    }
+
+    public static String dataCorrect(String arg){
+        String[] subStr;
+        String delimeter = "-";
+        subStr = arg.split(delimeter);
+        String newDate = subStr[2]+"/"+subStr[1]+"/"+subStr[0];
+        System.out.println(newDate);
+        return newDate;
     }
 }
